5b82197b8109d0af7d4618a40778a55b7b9270ff
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-09 22:17:17 -0400

    corrected num to num1 on line 4

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 def max_num(num1, num2, num3):
     if num1 >= num2 and num1 >= num3:
         return num1
-    elif num2 >= num and num2 >= num3:
+    elif num2 >= num1 and num2 >= num3:
         return num2
     else:
-        return num3    
+        return num3
+
+    print(max_num(8, 7, 5))
+
