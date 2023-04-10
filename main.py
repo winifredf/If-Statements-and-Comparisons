36b4028bb6fe4b78d54a58f083d6100c6c8caee1
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-09 22:11:47 -0400

    created if elif statements

---
+++ main.py
@@ -0,0 +1,7 @@
+def max_num(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num and num2 >= num3:
+        return num2
+    else:
+        return num3    
