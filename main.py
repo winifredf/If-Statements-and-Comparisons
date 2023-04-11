ee4e6b9b2d892e60562bc633ddecb5eccb7a56b6
Winifredf <Winifred.Dykes@hotmail.com>
2023-04-10 22:17:12 -0400

    Used not equal to != operator

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def max_num(num1, num2, num3):
-    if num1 == num2 and num1 >= num3:
+    if num1 != num2 and num1 >= num3:
         return num1
     elif num2 >= num1 and num2 >= num3:
         return num2
